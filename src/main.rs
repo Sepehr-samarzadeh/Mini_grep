3a4c10a3958386dc00fe7e1ca51ec704c492dd99
Sepehr Samarzadeh Vajdehfar <ssamarzadeh@gmail.com>
2025-03-13 00:15:06 +0100

    error handler has been added to config

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 use std::env;
+use std::env::args;
 use std::fs;
 fn main() {
     let args:Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
     //dbg!(args);
-
     println!("Searching for the {}",config.query);
     println!("in file {}",config.file_path);
 
     let contents = fs::read_to_string(config.file_path).expect("should have been able to read the file");
     println!("With text:\n {}",contents);
-
-
 }
 
 struct Config {
@@ -19,9 +17,23 @@ struct Config {
     file_path:String,
 }
 
-fn parse_config(args:&[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn new(args:&[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    Config{query,file_path}
+        Config{query,file_path}
+    }
 }
+
+
+
+
+
+
+
+
+
