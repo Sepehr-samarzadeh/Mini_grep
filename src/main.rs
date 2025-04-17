cf5e3a01f5d7470fbe83ce9b02ebe9c6a695417e
Sepehr Samarzadeh Vajdehfar <ssamarzadeh@gmail.com>
2025-04-17 12:50:42 +0200

    error handling has been added to project

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,27 @@
 use std::env;
-use std::env::args;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
+
 fn main() {
     let args:Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments:{err}");
+        process::exit(1);
+    });
     //dbg!(args);
     println!("Searching for the {}",config.query);
     println!("in file {}",config.file_path);
 
-    let contents = fs::read_to_string(config.file_path).expect("should have been able to read the file");
-    println!("With text:\n {}",contents);
-}
 
-struct Config {
-    query:String,
-    file_path:String,
-}
-
-impl Config {
-    fn new(args:&[String]) -> Config {
-        if args.len() < 3 {
-            panic!("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Config{query,file_path}
+    if let Err(e) = minigrep::run(config){
+        println!("Application error: {e}");
+        process::exit(1);
     }
+
+
 }
+
 
 
 
